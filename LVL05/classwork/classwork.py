67cf1b363b9dda63806b405ee8bdd2d134f5fcb6
datuashvilidani <datuashvilidani@gmail.com>
2025-03-06 14:56:58 +0100

    commit

---
+++ LVL05/classwork/classwork.py
@@ -0,0 +1,24 @@
+#აქ ცვლადში ვეკითხები მომხმარებელს თავის სახელს
+name=input("Tell me your name")
+#აქ ცვლადში ვეკითხები მომხარებელს თავის გვარს
+last_name=input("Tell me your last name")
+#აქ კი ასაკს იგივე გზით
+age=input("how old are you")
+
+#ფუნქცია არის ჩაშენებული ფუნქციები მაგ:print,str,int 
+
+
+# num1=int(input("your first num"))
+# num2=int(input("tell me your second num"))
+
+# print(num1+ num2)
+# print(num1 - num2)
+# print(num1*num2)
+# print(num1/num2)
+
+age=int(input("how old are you"))
+
+if age>=18:
+    print("You are allowed to enter")
+else:
+    print("you are not allowed to enter")
---
+++ LVL05/homework/homework.py
@@ -0,0 +1,29 @@
+# correct_password="DaniDatu"
+# password=input("Type in the password")
+# if password==correct_password:
+#     print("access granted")
+# else:
+#     print("access denied")
+
+
+# num=int(input("Tell me a number"))
+# if num%2==0:
+#     print("ეს ციფრი ლუწია")
+# else:
+#     print("ეს რიცხვი კენტია")
+
+# num1=int(input("Your 1 num"))
+# num2=int(input("Your 2 num"))
+# num3=int(input("Your 3 num"))
+# num4=int(input("Your 4 num"))
+# num5=int(input("Your 5 num"))
+
+# print(num1+num2+num3+num4+num5/)
+
+# c=int(input("Enter the temperature"))
+# f=c * 9/5 +32
+# print(f)
+
+f=int(input("Enter the temperature"))
+c=(f-32) * 5/9
+print(c)
