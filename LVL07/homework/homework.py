4bf4ce0713c161c0facc7e25e4db464c1f46443b
datuashvilidani <datuashvilidani@gmail.com>
2025-03-09 22:24:41 +0100

    commit

---
+++ LVL07/homework/homework.py
@@ -0,0 +1,26 @@
+#1.True and False:აქ გამოიტანს False
+#2.True or false:აქ გამოიტანს True
+
+
+#True and False or True or True and False:გამოიტანს True-ს
+
+# num1=int(input("tell me a number"))
+# num2=int(input("tell me second number"))
+
+# print(num1==num2)
+
+# score=int(input("Whats your score?"))
+# if score>=90:
+#     print("Grade A")
+# elif score>=70:
+#     print("Grade B")
+# elif score>=50:
+#     print("Grade C")
+# else:
+#     print("F")
+
+# number=int(input("your number?"))
+# if number%2==0 and number>10:
+#     print("True")
+# else:
+#     print("False")
