744df990196bb7a707880e4960c3332029073a75
datuashvilidani <datuashvilidani@gmail.com>
2025-03-12 15:40:00 +0100

    commit

---
+++ LVL08/classwork/classwork.py
@@ -0,0 +1,4 @@
+# num=int(input("your number"))
+
+# print(num >=5 and num<=10)
+
---
+++ LVL08/homework/homework.py
@@ -0,0 +1,19 @@
+#True and False or False or True and False:გამოპრინტავს False-ს
+
+# num=int(input("enter your number"))
+# if num%2==0 and num>=10:
+#     print("True")
+# else:
+#     print("False")
+
+
+#For loop მუშაობს ასე თუ გვინდა რომ 10 ჯერ გამოვპრინტოთ "გამარჯობა" არ გამოვიყენებთ print ფუნქციას ათჯერ ამ შემთხვევაში გამოიყენება for loop რომელიც აადვილებს ამ ყველაფრის დაწერას
+
+# for i in range(1,16):
+#     print(i)
+
+
+ghaf=0
+for i in range(1,11):
+    ghaf+=i
+    print(ghaf)
