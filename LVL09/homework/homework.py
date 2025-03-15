0885cba52ce6ef44b461be6830d0eb74a61bce5b
datuashvilidani <datuashvilidani@gmail.com>
2025-03-15 12:56:01 +0100

    commit

---
+++ LVL09/homework/homework.py
@@ -0,0 +1,16 @@
+# num=(input("Your name and surname"))
+# for i in num:
+#     print(i)
+
+
+# num=int(input("your number"))
+# for i in range(num):
+#     print(i)
+
+# num=int(input("enter number"))
+# for i in range(num,0,-1):
+#     print(i)
+
+#for loop არის sequence, for i in range(დაწყება,გაჩერება,სტეპი):ეს არის სამი ფუნქცია რაც range-ში შესაძლებელია რომ გამოიყენო, for loop-ს ასევე შეუძლია სტრინგებზეც მუშაობა
+
+#iteration არის გამეორებითი ცვლადი რომელიც იმეორებს ერთი და იგივე ბრძანებას,სიტყვას ან ფუნქციას უშეცდომოთ
