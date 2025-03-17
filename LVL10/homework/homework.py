3e3ce73e199af7d4300a3cbcce24f79a4ac77062
datuashvilidani <datuashvilidani@gmail.com>
2025-03-17 21:12:35 +0100

    commit

---
+++ LVL10/homework/homework.py
@@ -0,0 +1,18 @@
+# num1=15
+# num2=55
+# while num1<num2:
+#     num1+=5
+#     print(num1)
+#while ნიშნავს სანამ აქედან გამომდინარე ეს კოდი ამბობს ესეთ რამეს რომ სანამ num1 ნაკლებია num2-ზე, num1-ს მოუმატე 5 და იქამდე იტრიალებს ეს კოდი სანამ num1 არ იქნება num2-ზე მეტი და საბოლოოდ გამოპრინტავს იმ შედეგს სადაც num1 იქნება num2-ზე მეტი
+
+#for loop-ის და while loop-ი განსხვავდება ასე:for loop ის range-ში იწერება ისეთი რიცცხვი რომელიც ცნობილია ხოლო while loop-ს ვიყენებთ როცა რიცხვი უცნობია
+
+
+# correct_password="Dani123"
+# password=input("enter a password")
+# while password!=correct_password:
+#     password=input("your entered password was wrong please enter correct password")
+# print("access granted")
+
+for i in range(1,20,2):
+    print(i)
