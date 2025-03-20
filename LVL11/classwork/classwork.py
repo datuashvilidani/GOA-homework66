42e2d082a0de9786af22551296c6a2dd08510966
datuashvilidani <datuashvilidani@gmail.com>
2025-03-20 09:56:39 +0100

    commit

---
+++ LVL11/classwork/classwork.py
@@ -0,0 +1,9 @@
+# sum=0
+# for i in range(10):
+#     inputuser=int(input("enter a num"))
+#     sum+=inputuser
+# print(sum/10)
+
+
+
+
---
+++ LVL11/homework/homework.py
@@ -0,0 +1,36 @@
+# correct_email="danidatu@gmail.com"
+# correct_password="Dani123"
+# email=input("enter an email")
+# password=input("enter a password")
+# while password!=correct_password and email!=correct_email:
+#     password=input("your entered data was wrong please enter correct data")
+# print("access granted")
+
+
+
+# correctnum=9
+# num=int(input("Lets play a game where you will guess a number from 1-10"))
+# while num!=correctnum:
+#     num=int(input("nope thats not my number try again:))"))
+# print("great job you guessed it ")
+
+
+# correctnum=9
+# num=int(input("Lets play a game where you will guess a number from 1-10"))
+# if num!=correctnum:
+#     num=int(input("nope thats not my number try again:))"))
+# print("great job you guessed it ")
+
+
+# txt=input("enter a text")
+# for i in range(len(txt)):
+#     if i % 2==1:
+#         print(txt[i])
+
+# nums=[2,4,6,7,8,1,3]
+# allnum=0
+# for i in range(0,len(nums),2):
+#     allnum+=nums[i]
+#     print(allnum)
+
+        
