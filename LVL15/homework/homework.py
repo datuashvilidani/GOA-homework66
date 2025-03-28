7100bf542a573c6996e495fb6b2bb9ae40387b52
datuashvilidani <datuashvilidani@gmail.com>
2025-03-28 21:55:48 +0100

    commit

---
+++ LVL15/homework/homework.py
@@ -0,0 +1,12 @@
+# user=input("input a word")
+# print(user.upper())
+# print(user.lower())
+# print(user.capitalize())
+
+
+def something(sentence):
+    print(sentence.lower())
+    print(sentence.upper())
+    print(sentence.capitalize())
+sentence=input("enter a sentence")
+something(sentence)
